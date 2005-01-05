875683179daee5c73615bff46d9ef11142646308
Matthieu Herrb <matthieu.herrb@laas.fr>
2005-01-05 11:26:06 +0000

    add missing file

---
+++ canvas/server/posterLibStruct.h
@@ -0,0 +1,10 @@
+/*------------------  Fichier généré automatiquement ------------------*/
+/*------------------  Ne pas éditer manuellement !!! ------------------*/
+
+#ifndef $module$_POSTER_LIB_STRUCT_H
+#define $module$_POSTER_LIB_STRUCT_H
+
+$posterStruct$
+
+/*-------------------- Fin de chargement du fichier -----------------------*/
+#endif
