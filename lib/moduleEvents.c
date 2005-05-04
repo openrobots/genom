f37baa3ef4454864fe479d43cdbc59f3820e096b
Sylvain Joyeux <sjoyeux@laas.fr>
2005-05-04 11:54:01 +0000

    Removing all module event support but sendModuleEvent. Only send is used in simulation

--- lib/moduleEvents.c
+++ lib/moduleEvents.c
@@ -37,91 +37,8 @@ __RCSID("$LAAS$");
  *** pour la trace du comportement des modules
  ***/
 
-#ifdef VXWORKS
-#include <vxWorks.h>
-#include <semLib.h>
-#include <rngLib.h>
-#include <logLib.h>
-#else
-#include <portLib.h>
-#endif
-
-#include <stdio.h>
-
-#ifdef VXWORKS
-#include "../h2Clock/h2Clock.h" /* XXX utilise -I$(ROOTH2)/xxx/ */
-#endif
-
 #include "genom/moduleEvents.h"
 
-/*----------------------------------------------------------------------*/
-
-/**
- ** Variables statiques
- **/
-#ifdef VXWORKS
-static SEM_ID moduleEventSem;
-static RING_ID moduleEventRng;
-static BOOL moduleEventInitialized = FALSE;
-static BOOL moduleEventCapture = FALSE;
-#endif
-
-/* Timeout acces au semaphore d'exclusion mutuelle */
-#define MODULE_EVENT_TIMEOUT 5 
-
-/*----------------------------------------------------------------------*/
-
-/**
- ** moduleEventInit - initialisation de la collecte des evenements 
- **/
-STATUS
-moduleEventInit(int size)
-{
-#ifdef VXWORKS
-    if (moduleEventInitialized) {
-	return(ERROR);
-    }
-    moduleEventRng = rngCreate(size);
-    if (moduleEventRng == NULL) { 
-	return(ERROR);
-    }
-    moduleEventSem = semMCreate(SEM_Q_PRIORITY | SEM_INVERSION_SAFE);
-    if (moduleEventSem == NULL) {
-	rngDelete(moduleEventRng);
-	return(ERROR);
-    }
-#endif
-    return(OK);
-}
-
-#ifdef VXWORKS
-/*----------------------------------------------------------------------*/
-
-/**
- **  moduleEventStart - demarre l'acquisition des evenements
- **/
-STATUS 
-moduleEventStart(void)
-{
-    if (rngIsFull(moduleEventRng)) {
-	fprintf(stderr, "moduleEventStart: ring buffer full\n");
-	return(ERROR);
-    }
-    moduleEventCapture = TRUE;
-    return(OK);
-}
-
-/*----------------------------------------------------------------------*/
-
-/**
- ** moduleEventStop - arrete l'acquisition des evenements
- **/
-void 
-moduleEventStop(void)
-{
-    moduleEventCapture = FALSE;
-}
-#endif
 /*----------------------------------------------------------------------*/
 
 /**
@@ -133,62 +50,10 @@ moduleEventStop(void)
  ** retourne: la date en micro-secondes de l'evenement
  **/
 
-unsigned long
+void
 sendModuleEvent(MODULE_EVENT_STR *moduleEvent)
 {
-#ifdef VXWORKS
-    unsigned long stamp;
-
-    stamp = h2ClockuSec();
-    if (moduleEventCapture == FALSE) {
-	return(stamp);
-    }
-    moduleEvent->stamp = stamp;
-    if (semTake(moduleEventSem, MODULE_EVENT_TIMEOUT) == ERROR) {
-	logMsg("sendModuleEvent cannot take sem");
-	return(0);
-    }
-    rngBufPut(moduleEventRng, (char *)moduleEvent, sizeof(MODULE_EVENT_STR));
-    if (rngIsFull(moduleEventRng)) {
-	moduleEventCapture = FALSE;
-    }
-    semGive(moduleEventSem);
-    return(stamp);
-
-#else /* VXWORKS */
-    return(0);
-#endif /* VXWORKS */
 } /* sendModuleEvent */
 
 /*----------------------------------------------------------------------*/
-
-/**
- ** moduleEventDump - vide le buffer d'evenements dans un fichier
- **/
-#ifdef VXWORKS
-STATUS
-moduleEventDump(char *name)
-{
-    FILE *out;
-    MODULE_EVENT_STR event;
-
-    out = fopen(name, "w");
-    if (out == NULL) { 
-	return(ERROR);
-    }
-    moduleEventStop();
-    semTake(moduleEventSem, WAIT_FOREVER);
-    while (1) {
-	if (rngBufGet(moduleEventRng, (char *)&event, 
-		      sizeof(MODULE_EVENT_STR)) == 0) {
-	    break;
-	}
-	fwrite(&event, sizeof(MODULE_EVENT_STR), 1, out);
-    }
-    semGive(moduleEventSem);
-    fclose(out);
-    return(OK);
-}
-
-#endif
 
