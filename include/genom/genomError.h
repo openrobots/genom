ed5b9c523f4f57918c0d4e9b5ce902266f0f8d3a
Sara Fleury <sara@laas.fr>
2005-09-23 12:40:15 +0000

    New management of the error messages (see h2errorLib in pocolibs)

---
+++ include/genom/genomError.h
@@ -0,0 +1,72 @@
+/*	$LAAS$ */
+
+/* 
+ * Copyright (c) 1993-2003 LAAS/CNRS
+ * All rights reserved.
+ *
+ * Redistribution and use  in source  and binary  forms,  with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   1. Redistributions of  source  code must retain the  above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice,  this list of  conditions and the following disclaimer in
+ *      the  documentation  and/or  other   materials provided  with  the
+ *      distribution.
+ *
+ * THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
+ * INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#ifndef REPORTS_STD_H
+#define REPORTS_STD_H
+
+#include "h2errorLib.h"
+
+#define M_stdGenom 100
+
+#define S_stdGenom_ACTIVITY_INTERRUPTED          H2_ENCODE_STD_ERR(M_stdGenom, 1)
+#define S_stdGenom_TOO_MANY_ACTIVITIES           H2_ENCODE_STD_ERR(M_stdGenom, 2)
+#define S_stdGenom_ACTIVITY_FAILED               H2_ENCODE_STD_ERR(M_stdGenom, 3)
+#define S_stdGenom_WAIT_ABORT_ZOMBIE_ACTIVITY    H2_ENCODE_STD_ERR(M_stdGenom, 4)
+#define S_stdGenom_UNKNOWN_ACTIVITY              H2_ENCODE_STD_ERR(M_stdGenom, 5)
+#define S_stdGenom_FORBIDDEN_ACTIVITY_TRANSITION H2_ENCODE_STD_ERR(M_stdGenom, 6)
+#define S_stdGenom_SYSTEM_ERROR                  H2_ENCODE_STD_ERR(M_stdGenom, 7)
+#define S_stdGenom_ACTIVITY_ALREADY_ENDED        H2_ENCODE_STD_ERR(M_stdGenom, 8)
+#define S_stdGenom_WAIT_INIT_RQST                H2_ENCODE_STD_ERR(M_stdGenom, 9)
+#define S_stdGenom_CONTROL_CODEL_ERROR           H2_ENCODE_STD_ERR(M_stdGenom, 10)
+#define S_stdGenom_EXEC_TASK_SUSPENDED           H2_ENCODE_STD_ERR(M_stdGenom, 11)
+#define S_stdGenom_BAD_BLOCK_TYPE                H2_ENCODE_STD_ERR(M_stdGenom, 12)
+
+
+/* H2_ERROR[] */
+#define GENOM_H2_ERR_MSGS \
+{ \
+  {"ACTIVITY_INTERRUPTED",          S_stdGenom_ACTIVITY_INTERRUPTED}, \
+  {"TOO_MANY_ACTIVITIES",           S_stdGenom_TOO_MANY_ACTIVITIES}, \
+  {"ACTIVITY_FAILED",               S_stdGenom_ACTIVITY_FAILED}, \
+  {"WAIT_ABORT_ZOMBIE_ACTIVITY",    S_stdGenom_WAIT_ABORT_ZOMBIE_ACTIVITY}, \
+  {"UNKNOWN_ACTIVITY",              S_stdGenom_UNKNOWN_ACTIVITY}, \
+  {"FORBIDDEN_ACTIVITY_TRANSITION", S_stdGenom_FORBIDDEN_ACTIVITY_TRANSITION}, \
+  {"SYSTEM_ERROR",                  S_stdGenom_SYSTEM_ERROR}, \
+  {"ACTIVITY_ALREADY_ENDED",        S_stdGenom_ACTIVITY_ALREADY_ENDED}, \
+  {"WAIT_INIT_RQST",                S_stdGenom_WAIT_INIT_RQST}, \
+  {"CONTROL_CODEL_ERROR",           S_stdGenom_CONTROL_CODEL_ERROR},\
+  {"EXEC_TASK_SUSPENDED",           S_stdGenom_EXEC_TASK_SUSPENDED},	\
+  {"BAD_BLOCK_TYPE",                S_stdGenom_BAD_BLOCK_TYPE} \
+}
+
+extern const H2_ERROR genomH2errMsgs[]; /* it is GENOM_H2_ERR_MSGS */
+extern int genomRecordH2errMsgs();
+
+#endif
