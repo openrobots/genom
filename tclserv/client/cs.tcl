06df548407abb4ec31f25037f0574160226de675
Anthony Mallet <anthony.mallet@laas.fr>
2011-03-08 16:50:12 +0100

    Don't require pm::tools if eltclsh is present
    The el::base package from tclsh disappeared in eltclsh-1.11 and was replaced by
    a proper 'eltclsh' package. el::tools is still provided by eltclsh, so test for
    that instead of el::base.

--- tclserv/client/cs.tcl
+++ tclserv/client/cs.tcl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 1999-2003,2010 LAAS/CNRS
+# Copyright (c) 1999-2003,2010-2011 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use in source   and binary forms,  with or without
@@ -46,10 +46,8 @@ namespace eval cs {
     namespace export showtime
     namespace export verbose
 
-    if [catch {package present el::base}] {
+    if [catch {package present el::tools}] {
 	package require pm::tools
-    } else {
-	package require el::tools
     }
 
     # List server mailboxes ------------------------------------------------
