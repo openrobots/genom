b581934bf4ef86edfb2aa373b6727f485c757f9c
Matthieu Herrb <matthieu.herrb@laas.fr>
2013-02-22 14:58:32 +0100

    Remove unused variable

--- lib/instance.c
+++ lib/instance.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 CNRS/LAAS
+ * Copyright (c) 2012,2013 CNRS/LAAS
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -24,7 +24,6 @@
 #define GENOM_MAX_INSTANCE_NAME 256
 
 static char *genomInstance;
-static char *genomCntrlTaskInstance;
 
 static int genomInstanceInitialized = 0;
 
